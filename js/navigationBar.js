0468bf0fe8c41d6952ddc8e8434e17466e61084d
unknown <Yolanda@alisio.dia.fi.upm.es>
2014-11-18 17:01:05 +0100

    Improve dinamic browser: TODO change the style with ng-class directive.

--- js/navigationBar.js
+++ js/navigationBar.js
@@ -1,71 +1,35 @@
-// JavaScript Document
 var app = angular.module('toolController', ['ui.bootstrap']);
 
-app.controller('MainController', 
+app.controller('AccordionDemoCtrl', 
 function ($scope) {
-$scope.items = [
-                    {
-                        name: "item1",
-                        desc: "Item 1",
-                        subitems: [
-                            {
-                                name: "subitem1",
-                                desc: "Sub-Item 1"
-                            },
-                            {
-                                name: "subitem2",
-                                desc: "Sub-Item 2"
-                            },
-                            {
-                                name: "subitem2",
-                                desc: "Sub-Item 2"
-                            }]
-                    },
-                    {
-                        name: "item2",
-                        desc: "Item 2",
-                        subitems: [
-                            {
-                                name: "subitem1",
-                                desc: "Sub-Item 1"
-                            },
-                            {
-                                name: "subitem2",
-                                desc: "Sub-Item 2"
-                            },
-                            {
-                                name: "subitem2",
-                                desc: "Sub-Item 2"
-                            }]
-                    },
-                    {
-                        name: "item3",
-                        desc: "Item 3",
-                        subitems: [
-                            {
-                                name: "subitem1",
-                                desc: "Sub-Item 1"
-                            },
-                            {
-                                name: "subitem2",
-                                desc: "Sub-Item 2"
-                            },
-                            {
-                                name: "subitem2",
-                                desc: "Sub-Item 2"
-                            }]
-                    }
-                ];
 
-$scope.default = $scope.items[2];
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
 });
 
-app.controller('ItemController', ['$scope', function (scope) {
 
-                scope.$parent.isopen = (scope.$parent.default === scope.item);
 
-                scope.$watch('isopen', function (newvalue, oldvalue, scope) {
-                    scope.$parent.isopen = newvalue;
-                });
 
-            }]);
+
+
+
